cf5f0009c6a2e6e8811615ded29e20a053153d36
Turki alqasem <trk996@gmail.com>
2018-12-12 09:08:27 +0300

    revert

--- database_setup.py
+++ database_setup.py
@@ -6,8 +6,8 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__ = 'user'
+class Users(Base):
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
@@ -20,8 +20,8 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users)
 
     @property
     def serialize(self):
@@ -42,8 +42,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users)
 
     @property
     def serialize(self):
